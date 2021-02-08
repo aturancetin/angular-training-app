b9cb652bd27ef4dd76d9ed1284b44b5273a28364
aturancetin <aturancetinn@gmail.com>
2021-02-08 03:45:37 +0300

    Post card component created

---
+++ src/app/components/post-card/post-card.component.ts
@@ -0,0 +1,18 @@
+import { Post } from './../../models/Post';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-card',
+  templateUrl: './post-card.component.html',
+  styleUrls: ['./post-card.component.css']
+})
+export class PostCardComponent implements OnInit {
+
+  @Input() post!: Post;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
