48c6137f70ef4875b019b1c0e326a7011baa2480
aturancetin <aturancetinn@gmail.com>
2021-02-08 03:43:41 +0300

    Loading spinner component created

---
+++ src/app/components/loading/loading.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+
+
+
+
+@Component({
+  selector: 'app-loading',
+  template: `
+    <ng-lottie [styles]="styles" width="500px" [options]="options" (animationCreated)="animationCreated($event)"></ng-lottie>
+  `,
+  styleUrls: ['./loading.component.css']
+})
+export class LoadingComponent implements OnInit {
+  options: AnimationOptions = {
+    path: '/assets/loading.json',
+  }
+  styles: Partial<CSSStyleDeclaration> = {
+    maxWidth: '500px',
+    margin: '0 auto',
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
+
+}
