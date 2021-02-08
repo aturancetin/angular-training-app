dae4311f37963c258139ca83027e85fbdb42c262
aturancetin <aturancetinn@gmail.com>
2021-02-08 03:43:25 +0300

    Landing lottie component created

---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+
+
+@Component({
+  selector: 'app-landing',
+  template: `
+    <ng-lottie [styles]="styles" width="500px" [options]="options" (animationCreated)="animationCreated($event)"></ng-lottie>
+  `,
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent implements OnInit {
+  options: AnimationOptions = {
+    path: '/assets/landing.json',
+  }
+  styles: Partial<CSSStyleDeclaration> = {
+    maxWidth: '500px',
+
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
+
+}
