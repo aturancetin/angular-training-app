fad85f04822a48301ca53b4b73c1ef812c205498
aturancetin <aturancetinn@gmail.com>
2021-02-08 03:44:18 +0300

    Logo and navigation component created

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../service/auth.service'
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  token?:string | null = localStorage.getItem("token");
+  isLoggedIn?: boolean;
+
+  constructor(private router:Router, public authService: AuthService) { 
+    if(this.token) {
+      this.isLoggedIn = true;
+    }
+    else this.isLoggedIn = false;
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  isAuthenticated(){
+    return this.authService.isLoggedIn();
+  }
+
+  logout() {
+    
+    this.authService.logout();
+  }
+
+}
